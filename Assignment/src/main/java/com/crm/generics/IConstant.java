4f328600252fc5a5c7c1e9c4c085056aa27db8c0
prabhat19955 <77119514+prabhat19955@users.noreply.github.com>
2021-02-10 01:19:49 +0530

    Delete Assignment directory

--- Assignment/src/main/java/com/crm/generics/IConstant.java
+++
@@ -1,8 +0,0 @@
-package com.crm.generics;
-
-public interface IConstant {
-	public int implicitlyWiat = 20;
-	public int explicitlyWaitWiat = 20;
-	public String commonDataFilePath ="./Login.property";
-	public String excelFilePath ="./TestData.xlsx";
-}
